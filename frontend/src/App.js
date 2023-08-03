d4f749839f153b070cc95d1be9bb0b562b47715c
Carlos Martins <57817588+carlosbenoliel15@users.noreply.github.com>
2023-08-03 13:40:42 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <NavBar></NavBar>
+      <About/>
+      <SkillsBar/>
+      <Portfolio/>
+      <Contact/>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
